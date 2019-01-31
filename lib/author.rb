6d0cd832d89c11268413f11039381f680bf618c4
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-31 19:33:35 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author 
   attr_accessor :name 
   
+  @@post_count = 0
   def initialize(name)
     @name = name 
     @posts = []
@@ -13,12 +14,17 @@ class Author
   def add_post(post)
     @posts << post 
     post.author = self
+    @@post_count += 1 
   end 
   
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post 
     post.author = self
+    @@post_count += 1 
   end 
     
+  def self.post_count 
+    @@post_count
+  end 
 end 
