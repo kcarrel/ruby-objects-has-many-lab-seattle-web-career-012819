2fa3dba7d8c8645f301672213396e122a73be171
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-31 19:25:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist 
   
   attr_accessor :name
-  
+  @@count = 0
   def initialize(name)
     @name = name 
     @songs = []
@@ -14,12 +14,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self 
+    @@count += 1 
   end 
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
     song.artist = self
+    @@count += 1 
   end 
   
  
